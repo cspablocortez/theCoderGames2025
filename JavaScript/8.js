3958955a1938072b70826a7fe79eae874267cb4b
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2025-02-26 17:23:05 -0800

    p8

---
+++ JavaScript/8.js
@@ -0,0 +1,4 @@
+// Declare a variable and assign it to a string. The code should print "Yes" if
+// all the words in the string begin with an uppercase letter. Print "No"
+// otherwise. You cannot use a library for this solution.
+// (40pts)
